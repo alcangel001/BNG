f23ef49c6725aec2c26173063d8eeffe9606d5b3
alcangel001 <130193720+alcangel001@users.noreply.github.com>
2025-08-14 12:53:37 -0500

    Update settings.py media

--- bingo_project/settings.py
+++ bingo_project/settings.py
@@ -145,6 +145,10 @@ USE_I18N = True
 USE_TZ = True
 
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'credit_proofs')  # o cualquier carpeta donde guardes los uploads
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
