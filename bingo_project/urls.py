9ac38c096ee86af680de45a2df79c4818ce8f574
carlos45262 <loracarlosdaniel@gmail.com>
2025-08-14 16:01:26 -0500

    serve

--- bingo_project/urls.py
+++ bingo_project/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     # Admin
@@ -27,6 +28,7 @@ urlpatterns = [
     path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
         template_name='bingo_app/password_reset_complete.html'
     ), name='password_reset_complete'),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 
     
     
